fcc7c5073e06a250bd79a15a174f0714fbe39aea
suhaib zentuti <sohieb15@hotmail.com>
2019-11-19 20:21:24 +0000

    setup

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,19 @@
 
 // More Information about this assisngment is available in the README.md file
 
+for ( var i = 1; i <= 100; i++ ){
+
+    if( i % 3 == 0){
+        output("Fizz");
+    }
+    else if ( i % 5 ==0){
+        output("Buzz");
+    }
+    else if (i % 3 == 0 && i % 5 == 0){
+        output( "FizzBuzz");
+    }
+    else{
+        output(i);
+    }
+
+}
