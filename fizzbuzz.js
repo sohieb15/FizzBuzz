5e44e9eb8f65f5f0afb76bd531c9e6d23ad21e81
suhaib zentuti <sohieb15@hotmail.com>
2019-11-19 20:34:13 +0000

    setup3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,14 +3,14 @@
 
 for ( var i = 1; i <= 100; i++ ){
 
-    if ( i % 3 == 0){
+    if (i % 3 == 0 && i % 5 == 0){
+        output( "FizzBuzz");
+    }
+   else if ( i % 3 == 0){
         output("Fizz");
     }
     else if ( i % 5 == 0){
         output("Buzz");
-    }
-    else if (i % 3 == 0 && i % 5 == 0){
-        output( "FizzBuzz");
     }
     else{
         output(i);
